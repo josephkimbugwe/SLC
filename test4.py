fe2ee67b2bec595424ba098c7e4338c3d029fdd8
Kimbugwe Joseph <kimbugwejoseph91@gmail.com>
2017-04-18 13:43:08 -0700

    test 4 updated

--- test4.py
+++ test4.py
@@ -1,5 +1,7 @@
 import unittest
 from prime_number import prime_number
+
 class prime_number_test(unittest.TestCase):
+
     def test_arg_is_int(self):
         self.assertIsInstance(prime_number(1)[0], int, msg="Argument is not integer")
