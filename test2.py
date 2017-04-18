342b350d614d32d40adc3a4e982e719abe86784d
Kimbugwe Joseph <kimbugwejoseph91@gmail.com>
2017-04-18 13:40:24 -0700

    test two

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
 import unittest
 from prime_number import prime_number
 class prime_number_test(unittest.TestCase):
+#test for 3
     def test_prime_number_two(self):        
         self.assertEqual(prime_number(3)[1], [2,3], msg="Result is invalid")
+		
